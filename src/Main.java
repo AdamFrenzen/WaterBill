d42f5479ed3de329543738d7b14b701ac92de4c6
AdamFrenzen <adam.frenzen@gmail.com>
2026-09-14 14:52:12 -0600

    Using classes

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
 public class Main {
     
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        Customer charleeCustomer = new Customer();
+        Customer quinnCustomer = new Customer();
+
+        System.out.println(charleeCustomer.name);
+        System.out.println(quinnCustomer.gallonsUsed);
     }
 }
